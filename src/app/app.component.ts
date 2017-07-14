c00090d1e730cc8cc9b489760900badf4876be22
Jaime Valdivia <jaivaldivia@gmail.com>
2017-07-13 23:31:50 -0500

    Sort articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,10 @@ export class AppComponent {
     link.value = '';
     return false;
   }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort(
+      (a: Article, b: Article) => b.votes - a.votes
+    );
+  }
 }
